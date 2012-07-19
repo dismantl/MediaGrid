c1727600383f872fcecf0cd1cc04aa7f3c3d4b3f
Dan Staples <dan.staples@riseup.net>
2012-07-19 15:14:11 -0400

    ..

--- index.py
+++ index.py
@@ -1,29 +1,22 @@
-from copy import copy
-from pprint import pformat
 from twisted.internet.protocol import Protocol
-from pprint import pprint
 from twisted.web.resource import Resource
-from twisted.web.static import File
 from twisted.internet import reactor, task
 from twisted.web import server
 from twisted.web.template import Element, renderer, XMLFile, flatten, XMLString
-from twisted.python.filepath import FilePath
-from twisted.internet.defer import Deferred, DeferredList
+from twisted.internet.defer import Deferred
 from twisted.web.util import redirectTo
+from twisted.web.client import Agent
+from twisted.web.http_headers import Headers
 import cgi
 import json
-import urllib2
-import urllib
 import requests
-import tempfile
 import re
 import thread
-from time import asctime, localtime, sleep
-from twisted.web.client import getPage, Agent
-from twisted.web.http_headers import Headers
+from time import asctime, localtime
 
-host = '127.0.0.1:3456'
-port = 8080
+###### SETTINGS ######
+host = '127.0.0.1:3456'   # host/port of local Tahoe server
+port = 8080   # port that MediaGrid is listening on
 urlTemplate = 'http://' + host + '/uri'
 
 ####### SETUP #######
@@ -60,6 +53,7 @@ filetemp = XMLString(
     </tr>
     """)
 
+###### Template for sending new file listings to polling client ######
 class FileTemp(Element):
     def __init__(self, value, sub):
         Element.__init__(self, loader=filetemp)
@@ -84,6 +78,7 @@ class FileTemp(Element):
 		    time=asctime(localtime(self.value[itm][1]['metadata']['tahoe']['linkmotime'])),
 		    epoch=str(self.value[itm][1]['metadata']['tahoe']['linkmotime']))
 
+###### Template for file directory page ######
 class DirListing(Element):
     def __init__(self, value, sub, leafs):
         Element.__init__(self, loader=XMLFile(open('template.xml')))
@@ -130,6 +125,7 @@ class DirListing(Element):
 		    time=asctime(localtime(self.value[1]['children'][itm][1]['metadata']['tahoe']['linkmotime'])),
 		    epoch=str(self.value[1]['children'][itm][1]['metadata']['tahoe']['linkmotime']))
 
+###### Sends file data in chunks to client ######
 class printFile(Protocol):
     def __init__(self, request, finished):
         self.finished = finished
@@ -141,34 +137,55 @@ class printFile(Protocol):
     def connectionLost(self, reason):
         self.finished.callback(None)
 
-class PubDir(Resource):
+###### The main resource for handling client requests and interfacing with the Tahoe-LAFS server ######
+class MediaGrid(Resource):
 
-    def upload(self, sub, file, filename):
-	newfilename = filename
-	counter = 0
-	while True:
-	    #### Check if file of same name already exists
-	    req = (requests.get('%s/%s/%s/%s?t=json' % (urlTemplate, writecap, sub, newfilename)) if sub else requests.get('%s/%s/%s?t=json' % (urlTemplate, writecap, newfilename)))
-	    if req.content[0:13] == 'No such child': break
-	    parts = filename.split('.')
-	    newfilename = '%s_%d.%s' % (parts[0], counter, parts[1])
-	    counter += 1
-
-	##### UPLOAD FILE TO TAHOE GRID #########
-	req = (requests.put('%s/%s/%s/%s' % (urlTemplate, writecap, sub, newfilename), file) if sub else requests.put('%s/%s/%s' % (urlTemplate, writecap, newfilename), file))
+    def render_GET(self, request):
+	###### AJAX long-polling by client ######
+	if "poll" in request.args:
+	    sub = request.args["dir"][0]  # the current directory
+	    polltime = request.args["poll"][0]  # timestamp of newest file client has listed
+	    loop = task.LoopingCall(self.poll, polltime, request, sub)
+	    d = loop.start(5)
+	    def done(ignored):
+		try:
+		    loop.stop()
+		except:
+		    pass
+	    d.addErrback(done)
+ 	    crap = request.notifyFinish()
+	    crap.addBoth(lambda x: loop.stop())
+	    return server.NOT_DONE_YET
+	###### Return file contents ######
+	elif "file" in request.args:   
+	    d = Agent(reactor).request('GET', 'http://%s/file/%s' % (host, request.args["file"][0]))  # get file from Tahoe server
+	    d.addCallback(self.getFile, request)   # then send it on to the client
+	    d.addBoth(lambda x: request.finish())
+            return server.NOT_DONE_YET
+	###### List contents of directory ######
+	elif 'dir' in request.args:
+	    sub = request.args["dir"][0]   # current directory
+	    leafs = sub.split('/')
+	    return self.showDir(request, sub, leafs)
+	###### Start page ######
+	else:
+	    towrite = open('startpage.xml').read()
+	    return towrite
 
     def render_POST(self, request):
 	sub = request.args["when_done"][0]
 	leafs = sub.split('/')
 
+	###### Create directory ######
 	if request.args["t"][0] == 'mkdir':
 	    dirname = request.args["name"][0]
 	    req = (requests.put('%s/%s/%s/%s?t=mkdir' % (urlTemplate, writecap, sub, dirname)) if sub else requests.put('%s/%s/%s?t=mkdir' % (urlTemplate, writecap, dirname)))
 		
+	###### Upload file ######
 	elif request.args["t"][0] == 'upload':
 
+	    # Twisted.web doesn't know how to get the filename...here's a workaround:
 	    postheaders = request.getAllHeaders()
-
   	    postfile = cgi.FieldStorage(
 	        fp = request.content,
 	        headers = postheaders,
@@ -176,16 +193,25 @@ class PubDir(Resource):
 	                 'CONTENT_TYPE': postheaders['content-type'],
                 	}
 	        )
-
 	    filename = postfile["file"].filename
 	    if filename == '': filename = 'Untitled.txt'
-	    file = request.args["file"][0]
+
+	    file = request.args["file"][0]   # Fetch the file contents
 	    #alternative: file = postfile["file"].value
 
+	    # Upload file to Tahoe server in new thread so we can respond to client
 	    thread.start_new_thread(self.upload, (sub, file, filename))
 		
 	request.addCookie('uploaded','1')
 	return redirectTo('http://%s:%s/?dir=%s' % (request.getHost().host,port,sub),request)
+
+    def showDir(self, request, sub, leafs):
+	req = requests.get('%s/%s?t=json' % (url,sub))
+	jstuff = json.loads(req.text)
+	towrite = DirListing(jstuff, sub, leafs)
+	d = flatten(request, towrite, request.write)
+	d.addBoth(lambda x: request.finish())
+        return server.NOT_DONE_YET
 
     def getFile(self, response, request):
 	request.setHeader('Content-Length', response.length)
@@ -198,54 +224,34 @@ class PubDir(Resource):
         return finished
 
     def poll(self, polltime, request, sub):
-		req = requests.get('%s/%s?t=json' % (url,sub))
-		jstuff = json.loads(req.text)
-	        sortjstuff = sorted(jstuff[1]['children'], key=lambda x: jstuff[1]['children'][x][1]['metadata']['tahoe']['linkmotime'], reverse=True)
-		filelist = {}
-		for itm in sortjstuff:
-			if (polltime == '') or (round(jstuff[1]['children'][itm][1]['metadata']['tahoe']['linkmotime'],2) > float(polltime)):
-				filelist[itm] = jstuff[1]['children'][itm]
-			else: break
-		if filelist:
-		    towrite = FileTemp(filelist,sub)
-		    d = flatten(request, towrite, request.write)
-		    d.addCallback(lambda x: request.finish())
-		    return d
-
-    def render_GET(self, request):
-	if "poll" in request.args:
-	    sub = request.args["dir"][0]
-	    polltime = request.args["poll"][0]
-	    loop = task.LoopingCall(self.poll, polltime, request, sub)
-	    d = loop.start(5)
-	    def done(ignored):
-		try:
-		    loop.stop()
-		except:
-		    pass
-	    d.addErrback(done)
- 	    crap = request.notifyFinish()
-	    crap.addBoth(lambda x: loop.stop())
-	    return server.NOT_DONE_YET
-	if "file" in request.args:
-	    d = Agent(reactor).request('GET', 'http://%s/file/%s' % (host, request.args["file"][0]))
-	    d.addCallback(self.getFile, request)
-	    d.addBoth(lambda x: request.finish())
-            return server.NOT_DONE_YET
-	if 'dir' in request.args:
-	    sub = request.args["dir"][0]
-	    leafs = sub.split('/')
-	    return self.showDir(request, sub, leafs)
-	else:
-	    towrite = open('startpage.xml').read()
-	    return towrite
-
-    def showDir(self, request, sub, leafs):
-	req = requests.get('%s/%s?t=json' % (url,sub))
+	req = requests.get('%s/%s?t=json' % (url,sub))   # Get json info of directory contents
 	jstuff = json.loads(req.text)
-	towrite = DirListing(jstuff, sub, leafs)
-	d = flatten(request, towrite, request.write)
-	d.addBoth(lambda x: request.finish())
-        return server.NOT_DONE_YET
+	sortjstuff = sorted(jstuff[1]['children'], 
+		key=lambda x: jstuff[1]['children'][x][1]['metadata']['tahoe']['linkmotime'], 
+		reverse=True)   # sort for newest first
+	filelist = {}
+	for itm in sortjstuff:
+	    if (polltime == '') or (round(jstuff[1]['children'][itm][1]['metadata']['tahoe']['linkmotime'],2) > float(polltime)):   # is file newer than what the client has?
+		filelist[itm] = jstuff[1]['children'][itm]
+	    else: break
+	if filelist:   # if we found newer files, send info to client
+	    towrite = FileTemp(filelist,sub)
+	    d = flatten(request, towrite, request.write)
+	    d.addCallback(lambda x: request.finish())
+	    return d
 
-resource = PubDir()
+    ###### Upload file to Tahoe server ######
+    def upload(self, sub, file, filename):
+	newfilename = filename
+	counter = 0
+	while True:
+	    # Check if file of same name already exists
+	    req = (requests.get('%s/%s/%s/%s?t=json' % (urlTemplate, writecap, sub, newfilename)) if sub else requests.get('%s/%s/%s?t=json' % (urlTemplate, writecap, newfilename)))
+	    if req.content[0:13] == 'No such child': break
+	    parts = filename.split('.')
+	    newfilename = '%s_%d.%s' % (parts[0], counter, parts[1])
+	    counter += 1
+
+	req = (requests.put('%s/%s/%s/%s' % (urlTemplate, writecap, sub, newfilename), file) if sub else requests.put('%s/%s/%s' % (urlTemplate, writecap, newfilename), file))   # upload the damn thing
+
+resource = MediaGrid()
